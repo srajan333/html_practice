07cf9254fff8ed2a8a8eaa93fa92f39ff7fc46ed
srajan333 <158080328+srajan333@users.noreply.github.com>
2025-02-12 18:40:38 +0530

    Practice HTML

---
+++ abc.js
@@ -0,0 +1,6 @@
+let btn=document.querySelector('button');
+btn.addEventListener("click",()=>{
+    // btn.style.backgroundColor="teal";
+    btn.style.backgroundColor="green";
+    alert("do not press it");
+})
